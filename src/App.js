cb2c5f656fa073b28133de4b143be40c989e2977
Ics335572 <ahmedgulfam113@gmail.com>
2024-09-05 00:21:29 +0500

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import Signup from './components/Signup';
 import Login from './components/Login';
 import './App.css';
 import logo from './logo.svg';
+import Deshboard from './components/userPanal/Deshboard';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Devices from './components/userPanal/Devices';
+import News from './components/userPanal/News';
+import DeviceView from './components/userPanal/DeviceView';
+import Request from './components/userPanal/Request';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Header />
         {/* <header className="App-header">
           <Link to="/">
             <img src={logo} className="App-logo" alt="logo" />
@@ -18,7 +26,14 @@ function App() {
           <Route path="/" element={<Signup />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
+          <Route path='/deshboard' element={<Deshboard />} />
+          <Route path='/devices' element={<Devices />} />
+          <Route path='/unbrick' element={<Devices />} />
+          <Route path='/devices/view' element={<DeviceView />} />
+          <Route path='/news' element={<News />} />
+          <Route path='/request' element={<Request />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
---
+++ src/components/userPanal/Request.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Request() {
+    return (
+        <div class="min-h-screen flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: `url('https://images.unsplash.com/photo-1512941937669-90a1b58e7e9c?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')` }}>
+            <div class="text-center p-8 bg-white bg-opacity-50 backdrop-blur-lg rounded-lg max-w-2xl mx-auto">
+                <h1 class="text-6xl font-bold text-gray-900">COMING SOON</h1>
+                <p class="mt-4 text-lg text-gray-700">We are working hard to bring you the new mobile version. Stay tuned for exciting new features and builds that will enhance your experience. Coming soon to all platforms.</p>
+            </div>
+        </div>
+
+    )
+}
+
+export default Request
